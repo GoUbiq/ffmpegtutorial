bbae3ec4ecc7dc352dafaf9a5995538d1df21077
Praj <prajna.kandarpa@uwaterloo.ca>
2015-03-20 03:44:11 -0400

    progress

--- test.c
+++ test.c
@@ -3,104 +3,241 @@
 #include <libswscale/swscale.h>
 #include <libavdevice/avdevice.h>
 #include <libavutil/opt.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/common.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/samplefmt.h>
-#include <x264.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define HTTP_ADDRESS "52.11.123.78"
+#define HTTP_PORT    8080
 
 // void show_av_device() {
 
-//    ifmt->get_device_list(pFormatCtx, device_list);
+//    inFmt->get_device_list(inFmtCtx, device_list);
 //    printf("Device Info=============\n");
-//    //avformat_open_input(&pFormatCtx,"video=Capture screen 0",ifmt,&options);
+//    //avformat_open_input(&inFmtCtx,"video=Capture screen 0",inFmt,&inOptions);
 //    printf("===============================\n");
 // }
 
+void registerLibs() {
+    av_register_all();
+    avdevice_register_all();
+    avformat_network_init();
+    avcodec_register_all();
+}
+
+void setupInputVideostream(AVFormatContext* inFmtCtx, const char *videoDeviceName) {
+    
+    
+    
+    
+}
+
+void setupInputAudiostream(AVFormatContext* );
+
+
 int main(int argc, char *argv[]) {
-  AVFormatContext   *pFormatCtx = NULL;
-  int               i, videoStream;
-  AVCodecContext    *pCodecCtx = NULL;
-  AVCodec           *pCodec = NULL;
-  AVDictionary      *options = NULL;
-  // AVDeviceInfoList *device_list = NULL;
-  AVInputFormat     *ifmt;
-  const char        *name;
-  AVFrame           *pFrame = NULL;
-  AVFrame           *pFrameYUV = NULL;
-  int               numBytes;
-  // AVFrame *pFrameRGB = NULL;
-  // uint8_t *buffer = NULL;
-  // int numBytes;
-  // struct SwsContext *swsCtx = NULL;
-  // int frameFinished;
-  // AVPacket packet;
-
-  name = "avfoundation";
-  av_register_all();
-  avdevice_register_all();
-  pFormatCtx = avformat_alloc_context();
-
-  // av_dict_set(&options, "list_devices", "true", 0);
-  av_dict_set(&options, "pixel_format", "uyvy422", 0);
-  av_dict_set(&options, "probesize", "7000000", 0);
-
-  ifmt = av_find_input_format(name);
-  if (avformat_open_input(&pFormatCtx, "Capture screen 0", ifmt, &options) != 0) {
-    printf("Could not load the context for the input device\n");
-    return -1;
-  }
-  if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
-    printf("Could not find stream info for screen\n");
-    return -1;
-  }
-  av_dump_format(pFormatCtx, 0, "Capture screen 0", 0);
-  // pFormatCtx->streams is an array of pointers of size pFormatCtx->nb_streams
-  // so loop through each stream and find a video stream
-  videoStream = -1;
-  for (i = 0; i < pFormatCtx->nb_streams; i++) {
-    if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-      videoStream = i;
-      break;
+    
+    //conversion variables
+    struct SwsContext *swsCtx = NULL;
+    //input stream variables
+    AVFormatContext   *inFmtCtx = NULL;
+    AVCodecContext    *inCodecCtx = NULL;
+    AVCodec           *inCodec = NULL;
+    AVInputFormat     *inFmt = NULL;
+    AVFrame           *inFrame = NULL;
+    AVDictionary      *inOptions = NULL;
+    const char *streamURL = "http://localhost:8090/feed1.ffm";
+    const char *name = "avfoundation";
+    
+//    AVFrame           *inFrameYUV = NULL;
+    AVPacket          inPacket;
+    
+    
+    //output stream variables
+    AVCodecContext    *outCodecCtx = NULL;
+    AVCodec           *outCodec = NULL;
+    AVFormatContext   *outFmtCtx = NULL;
+    AVOutputFormat    *outFmt = NULL;
+    AVFrame           *outFrameYUV = NULL;
+    AVStream          *stream;
+    
+    int               i, videostream;
+    int               numBytes, frameFinished;
+    
+    registerLibs();
+    inFmtCtx = avformat_alloc_context(); //alloc input context
+    av_dict_set(&inOptions, "pixel_format", "uyvy422", 0);
+    av_dict_set(&inOptions, "probesize", "7000000", 0);
+    
+    inFmt = av_find_input_format(name);
+    if (avformat_open_input(&inFmtCtx, "Capture screen 0", inFmt, &inOptions) != 0) {
+        printf("Could not load the context for the input device\n");
+        return -1;
     }
-  }
-  if (videoStream == -1) {
-    printf("no video stream found\n");
-    return -1;
-  }
+    if (avformat_find_stream_info(inFmtCtx, NULL) < 0) {
+        printf("Could not find stream info for screen\n");
+        return -1;
+    }
+    av_dump_format(inFmtCtx, 0, "Capture screen 0", 0);
+    // inFmtCtx->streams is an array of pointers of size inFmtCtx->nb_stream
+    
+    videostream = av_find_best_stream(inFmtCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &inCodec, 0);
+    if (videostream == -1) {
+        printf("no video stream found\n");
+        return -1;
+    } else {
+        printf("%s is inCodec\n", inCodec->long_name);
+    }
+    inCodecCtx = inFmtCtx->streams[videostream]->codec;
+    // open codec
+    if (avcodec_open2(inCodecCtx, inCodec, NULL) > 0) {
+        printf("Couldn't open codec");
+        return -1;  // couldn't open codec
+    }
 
-  pCodecCtx = pFormatCtx->streams[videoStream]->codec;
-  pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-  if (pCodec == NULL) {
-    fprintf(stderr, "Unsupported codec\n");
-    return -1;
-  }
-  // open codec
-  if (avcodec_open2(pCodecCtx, pCodec, NULL) > 0) {
-    return -1;  // couldn't open codec
-  }
-  // Allocate video frame
-  pFrame = av_frame_alloc();
-  numBytes = avpicture_get_size(PIX_FMT_UYVY422, pCodecCtx->width, pCodecCtx->height);
-  uint8_t *buffer = (uint8_t *)av_malloc(
-    numBytes*sizeof(uint8_t));
+    
+        //setup output params
+    outFmt = av_guess_format(NULL, streamURL, NULL);
+    if(outFmt == NULL) {
+        printf("output format was not guessed properly");
+        return -1;
+    }
+    
+    if((outFmtCtx = avformat_alloc_context()) < 0) {
+        printf("output context not allocated. ERROR");
+        return -1;
+    }
+    
+    printf("%s", outFmt->long_name);
+    
+    outFmtCtx->oformat = outFmt;
+    
+    snprintf(outFmtCtx->filename, sizeof(outFmtCtx->filename), streamURL);
+    printf("%s\n", outFmtCtx->filename);
+    
+    stream = avformat_new_stream(outFmtCtx, outCodec);
+    outCodecCtx = stream->codec;
+    
+    outCodecCtx->codec_id = CODEC_ID_H264;
+    outCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+    outCodecCtx->flags = CODEC_FLAG_GLOBAL_HEADER;
+    outCodecCtx->width = inCodecCtx->width;
+    outCodecCtx->height = inCodecCtx->height;
+    outCodecCtx->time_base.den = 25;
+    outCodecCtx->time_base.num = 1;
+    outCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+    outCodecCtx->gop_size = 50;
+    outCodecCtx->bit_rate = 400000;
+    
+    if (avio_open(&outFmtCtx->pb, outFmtCtx->filename, AVIO_FLAG_READ_WRITE ) < 0) {
+        perror("url_fopen failed");
+    }
+    
 
-  int ret = avpicture_fill((AVPicture *)pFrame, buffer, PIX_FMT_UYVY422,
-                pCodecCtx->width, pCodecCtx->height);
+    
+    if (avformat_write_header(outFmtCtx, NULL) != 0) {
+        printf("was not able to write header to outout format");
+        return -1;
+    }
+    
+    
+    numBytes = avpicture_get_size(PIX_FMT_UYVY422, inCodecCtx->width, inCodecCtx->height);
+    // Allocate video frame
+    inFrame = av_frame_alloc();
 
-  printf("%d is return val for fill\n", ret);
+   
+    
+    swsCtx = sws_getContext(inCodecCtx->width, inCodecCtx->height, inCodecCtx->pix_fmt, inCodecCtx->width,
+                            inCodecCtx->height, PIX_FMT_YUV420P, SWS_BILINEAR, NULL, NULL, NULL);
+    int frame_count = 0;
+    while(av_read_frame(inFmtCtx, &inPacket) >= 0) {
+        if(inPacket.stream_index == videostream) {
+            avcodec_decode_video2(inCodecCtx, inFrame, &frameFinished, &inPacket);
+            // 1 Frame might need more than 1 packet to be filled
+            if(frameFinished) {
+                outFrameYUV = av_frame_alloc();
+                
+                uint8_t *buffer = (uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+                
+                int ret = avpicture_fill((AVPicture *)outFrameYUV, buffer, PIX_FMT_YUV420P,
+                                         inCodecCtx->width, inCodecCtx->height);
+                if(ret < 0){
+                    printf("%d is return val for fill\n", ret);
+                    return -1;
+                }
+                //convert image to YUV
+                sws_scale(swsCtx, (uint8_t const * const* )inFrame->data,
+                          inFrame->linesize, 0, inCodecCtx->height,
+                          outFrameYUV->data, outFrameYUV->linesize);
+                //outFrameYUV now holds the YUV scaled frame/picture
+                
+                AVPacket pkt;
+                int got_output;
+                av_init_packet(&pkt);
+                pkt.data = NULL;
+                pkt.size = 0;
 
-  // if (avpicture_fill((AVPicture *)pFrameYUV, buffer, PIX_FMT_UYVY422,
-  //               pCodecCtx->width, pCodecCtx->height) != 0) {
+                outFrameYUV->pts = 25;
+                
+                ret = avcodec_encode_video2(outFmtCtx->streams[0]->codec, &pkt, outFrameYUV, &got_output);
+                if (ret < 0) {
+                    fprintf(stderr, "Error encoding video frame: %s\n", av_err2str(ret));
+                    return -1;
+                }
+                
+                if(got_output) {
+                    if(stream->codec->coded_frame->key_frame) {
+                        pkt.flags |= AV_PKT_FLAG_KEY;
+                    }
+                    pkt.stream_index = stream->index;
+                    if(pkt.pts != AV_NOPTS_VALUE)
+                        pkt.pts = av_rescale_q(pkt.pts, stream->codec->time_base, stream->time_base);
+                    if(pkt.dts != AV_NOPTS_VALUE)
+                        pkt.dts = av_rescale_q(pkt.dts, stream->codec->time_base, stream->time_base);
+                    ret = av_interleaved_write_frame(outFmtCtx, &pkt);
+                } else {
+                    ret = 0;
+                }
+                if(ret != 0) {
+                    fprintf(stderr, "Error while writing video frame: %s\n", av_err2str(ret));
+                    exit(1);
+                }
+                
+                fprintf(stderr, "encoded frame #%d\n", frame_count);
+                frame_count++;
+                
+                av_free_packet(&pkt);
+                av_free(outFrameYUV);
+                
+            }
+        }
+        av_free_packet(&inPacket);
+    }
+    av_write_trailer(outFmtCtx);
+    
+    //close video stream
+    if(stream) {
+        avcodec_close(outCodecCtx);
+    }
+    for (i = 0; i < outFmtCtx->nb_streams; i++) {
+        av_freep(&outFmtCtx->streams[i]->codec);
+        av_freep(&outFmtCtx->streams[i]);
+    }
+    if (!(outFmt->flags & AVFMT_NOFILE))
+    /* Close the output file. */
+        avio_close(outFmtCtx->pb);
+    /* free the output format context */
+    avformat_free_context(outFmtCtx);
+    
+    // Free the YUV frame populated by the decoder
+    av_free(inFrame);
+    
+    // Close the video codec (decoder)
+    avcodec_close(inCodecCtx);
+    
+    // Close the input video file
+    avformat_close_input(&inFmtCtx);
 
-  // }
+    return 1;
 
-
-
-  return 1;
-
-  // convert frame from native format to 24 bit RGB(PPM format)
-  // also allocate a frame for the converted frame
 }
